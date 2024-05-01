20efd2b5c9a3d073de872c91983107eb5bb373d7
VinGitonga <gitongavincent64@gmail.com>
2024-05-01 15:16:47 +0300

    Add meta tags and service worker registration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,26 @@
+import { useEffect } from "react";
 import AppProviders from "./providers/AppProviders";
+import { getSerwist } from "virtual:serwist";
 
 const App = () => {
+	useEffect(() => {
+		/**
+		 * Load the service worker and register it
+		 */
+		const loadSerwist = async () => {
+			if ("serviceWorker" in navigator) {
+				const serwist = await getSerwist();
+
+				serwist?.addEventListener("installed", () => {
+					console.log("Service Worker Installed");
+				});
+
+				void serwist?.register();
+			}
+		};
+
+		loadSerwist();
+	}, []);
 	return <AppProviders />;
 };
 
--- client/src/components/app-logo/AppLogo.tsx
+++ client/src/components/app-logo/AppLogo.tsx
@@ -1,12 +1,12 @@
-import { SiMagic } from "react-icons/si";
 import { Link } from "react-router-dom";
+import { TbRadioactive } from "react-icons/tb";
 
 const AppLogo = () => {
 	return (
 		<Link to={"/"}>
 			<div className="flex items-center space-x-2" data-cy="app-logo">
 				<div className="p-2 bg-[#ff9900] rounded-full">
-					<SiMagic />
+					<TbRadioactive className="text-white w-6 h-6" />
 				</div>
 				<h1 className="text-3xl font-bold">Nitron</h1>
 			</div>
